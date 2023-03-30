fbca428fe2df9e5cdbec4a74723a6e03c556a56a
student <awipfjapwfj@sadifseif.com>
2023-03-30 13:47:13 +0200

    lab2 cdn

--- lab_2/Server.java
+++ lab_2/Server.java
@@ -2,25 +2,45 @@ package lab_2;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 
 public class Server {
     public static void main(String[] argv) throws Exception {
-        String clientSentence;
-        String capitalizedSentence;
+        String clientSentence = "";
+        String outToClientSentence = "";
+        ServerSocket welcomeSocket = null;
+        DataInputStream inFromClient = null;
+        DataOutputStream outToClient = null;
+        byte[] answer = new byte[100];
         try {
-            ServerSocket welcomeSocket = new ServerSocket(6789);
-        }catch (Exception e){
+            welcomeSocket = new ServerSocket();
+
+        } catch (Exception e) {
             System.out.println("Can't initialize server");
         }
+        assert welcomeSocket != null;
+        Socket connectionSocket = null;
+        try {
+            welcomeSocket.bind(new InetSocketAddress(7));
+        } catch (Exception e) {
+            System.out.println("Can't bind server socket");
+        }
         while (true) {
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient =
-                    new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-            clientSentence = inFromClient.readLine();
-            System.out.println("Received: " + clientSentence);
-            capitalizedSentence = clientSentence.toUpperCase() + 'n';
-            outToClient.writeBytes(capitalizedSentence);
+            connectionSocket = welcomeSocket.accept();
+            while (true) {
+                try {
+                    assert connectionSocket != null;
+                    inFromClient = new DataInputStream(connectionSocket.getInputStream());
+                    outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                } catch (Exception e) {
+                    break;
+                }
+                int answerSize = inFromClient.read(answer);
+                if (answerSize == -1) break;
+                System.out.println("Message: " + new String(answer,0,answerSize));
+                System.out.println("Size: " + answerSize);
+                outToClient.write(answer, 0, answerSize);
+            }
         }
     }
 }

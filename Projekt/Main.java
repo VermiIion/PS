a2989206b6b99e264c0a8ecb44af209531088c82
VermiIion <misterpig231@gmail.com>
2024-02-20 18:53:47 +0100

    dada

--- Projekt/Main.java
+++ Projekt/Main.java
@@ -3,6 +3,7 @@ package Projekt;
 
 import org.json.JSONObject;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -12,10 +13,15 @@ public class Main {
         LicenceClient client = new LicenceClient();
         String ip = "";
         int port;
-        System.out.println("Wpisz IP");
-        ip = scanner.nextLine();
-        System.out.println("Wpisz port");
-        port = scanner.nextInt();
+        try {
+            System.out.println("Wpisz IP");
+            ip = scanner.nextLine();
+            System.out.println("Wpisz port");
+            port = scanner.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("Niewłaściwy Port");
+            System.exit(1);
+        }
         client.start("localhost",8080);
         // Admin E3AFED0047B08059D0FADA10F400C1E5
         // Radek 9F3A08745C23449A53FC05D68EDA1E1B
